0d444f1a9fb5be9961e92d4180a82ae224ddff7a
juliettebras <juliettebras@localhost>
2022-10-25 17:07:11 +0200

    Projet 1 complété

---
+++ TP2_Bieres_BRAS/src/tp2_bieres_bras/TP2_Bieres_BRAS.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_bras;
+
+/**
+ *
+ * @author juliettebras
+ */
+public class TP2_Bieres_BRAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+  
+BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",
+7.0 ,"Dubuisson");
+System.out.println(uneBiere);
+
+ //BouteilleBiere uneBiere = new BouteilleBiere();
+ 
+ //uneBiere.nom = "Cuvée des trolls"; 
+ //uneBiere.degreAlcool = 7.0 ;
+ //uneBiere.brasserie ="Dubuisson";
+ //uneBiere.ouverte= false;
+ //uneBiere.lireEtiquette();
+BouteilleBiere secondeBiere= new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe");
+
+        
+ //BouteilleBiere secondeBiere = new BouteilleBiere();
+ //secondeBiere.nom="Leffe";
+ //secondeBiere.degreAlcool=6.6;
+ //secondeBiere.brasserie="Abbaye de Leffe";
+ //uneBiere.lireEtiquette();
+BouteilleBiere troisiemeBiere= new BouteilleBiere("Heineken", 5.6,"Abbaye de Heineken");
+BouteilleBiere quatriemeBiere= new BouteilleBiere("Corona",7.3,"Abbaye du Corona");
+  
+uneBiere.Decapsuler();
+System.out.println(uneBiere);
+
+        
+ }
+ 
+}
