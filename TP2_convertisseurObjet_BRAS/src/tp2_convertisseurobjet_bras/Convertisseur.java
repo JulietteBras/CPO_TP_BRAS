4f99a4c7ff7259452e25ee712edc1c87cda2dc44
juliettebras <juliettebras@localhost>
2022-10-30 19:41:52 +0100

    Commentaires partie 2

--- TP2_convertisseurObjet_BRAS/src/tp2_convertisseurobjet_bras/Convertisseur.java
+++ TP2_convertisseurObjet_BRAS/src/tp2_convertisseurobjet_bras/Convertisseur.java
@@ -11,55 +11,55 @@ import java.util.Scanner;
  *
  * @author juliettebras
  */
-public class Convertisseur {
-    int nbConversions;
+public class Convertisseur { //Classe Convertisseur
+    int nbConversions; //permet de compter le nombre de conversions qui seront effectuées par le convertisseur
 
     
-    public Convertisseur () {
+    public Convertisseur () { //Constructeur Convertisseur avec initialisation de nbConversions
         nbConversions = 0;
         
 }
-    public double CelciusVersKelvin(double CVK){
+    public double CelciusVersKelvin(double CVK){ //Methode conversion Celcius vers Kelvin
         CVK = CVK + 273.15;
         nbConversions += 1;
         return CVK;
     }
    
-    public double KelvinVersCelscius (double KVC){
+    public double KelvinVersCelscius (double KVC){ //Méthode conversion Celcius vers Kelvin
         KVC = KVC - 273.15;
         nbConversions += 1;
         return KVC;
     }
    
-    public double FarenheitVersCelcius (double FVC){
+    public double FarenheitVersCelcius (double FVC){ //Méthode conversion Farenheit vers Celcius
         FVC = (FVC - 32)*(5/9);
         nbConversions += 1;
         return FVC;
     }
    
-    public double CelciusVersFarenheit (double CVF){
+    public double CelciusVersFarenheit (double CVF){ //Méthode conversion Celcius vers Farenheit
         CVF = CVF*(9/5)+32;
         nbConversions += 1;
         return CVF;
         }
-    public double FarenheitVersKelvin (double FVK){
+    public double FarenheitVersKelvin (double FVK){ //Méthode conversion Farenheit vers Kelvin
         FVK=(FVK-32)*(5/9)+273.15;
         nbConversions += 1;
         return FVK;
         }
-    public double KelvinVersFarenheit (double KVF){
+    public double KelvinVersFarenheit (double KVF){ //Methode conversion Kelvin vers Farenheit
         KVF=(KVF-273.15)*(9/5)+32;
         nbConversions += 1;
         return KVF;
         }
     @Override
-public String toString () {
+public String toString () { //Méthode toString retourne le nombre de conversions effectuées
   return "nb de conversions "+ nbConversions;
 }
 public int choisir()  { 
   System.out.println("1. Celcius vers Kelvin \n"+ "2. Kelvin vers Celcius\n"+"3. Farenheit vers Celcius \n"+ "4. Celcius vers Farenheit \n"+ "5. Farenheit vers Kelvin \n"+ "6. Kelvin vers Farenheit \n");
     
-    Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in); //Demander à l'utilisateur son choix de conversion
     int choix=sc.nextInt();
     return choix;
   }
--- TP2_convertisseurObjet_BRAS/src/tp2_convertisseurobjet_bras/TP2_convertisseurObjet_BRAS.java
+++ TP2_convertisseurObjet_BRAS/src/tp2_convertisseurobjet_bras/TP2_convertisseurObjet_BRAS.java
@@ -17,42 +17,51 @@ public class TP2_convertisseurObjet_BRAS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Convertisseur premier =new Convertisseur();
+        
+    Convertisseur premier =new Convertisseur(); //Objet qui effectue deux types de conversion
     double result1= premier.CelciusVersKelvin(4);
     double result2= premier.FarenheitVersKelvin(4);
     System.out.println(result1);
     System.out.println(result2);
     System.out.println(premier);
     
-    Convertisseur deuxieme =new Convertisseur();
+    Convertisseur deuxieme =new Convertisseur(); //Objet qui effectue deux types de conversion
     double result3= deuxieme.KelvinVersCelscius(9);
     double result4=deuxieme.KelvinVersFarenheit(9);
     System.out.println(result3);
     System.out.println(result4);
     System.out.println(deuxieme);
     
+    //Menu pour l'utilisateur
+    
     Convertisseur ConvertChoisie= new Convertisseur();
     
-    System.out.println("1. Celcius vers Kelvin \n"+ "2. Kelvin vers Celcius\n"+"3. Farenheit vers Celcius \n"+ "4. Celcius vers Farenheit \n"+ "5. Farenheit vers Kelvin \n"+ "6. Kelvin vers Farenheit \n");
+    System.out.println("Choisir une conversion :\n" +"1. Celcius vers Kelvin \n"+ "2. Kelvin vers Celcius\n"+"3. Farenheit vers Celcius \n"+ "4. Celcius vers Farenheit \n"+ "5. Farenheit vers Kelvin \n"+ "6. Kelvin vers Farenheit \n"+"Rentrez un nombre :");
     
-    Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in); //Demander à l'utilisateur la valeur pour sa conversion
     double nombre=sc.nextDouble();
     int choix= ConvertChoisie.choisir();
+    
     if (choix==1){
         double nbfinal =ConvertChoisie.CelciusVersKelvin(nombre);
         System.out.println(nbfinal);
+        
         }else if (choix==2) {
         double nbfinal =ConvertChoisie.KelvinVersCelscius(nombre);
         System.out.println(nbfinal);
+        
         }else if (choix==3) {
         double nbfinal =ConvertChoisie.FarenheitVersCelcius(nombre);
         System.out.println(nbfinal);
+        
         }else if (choix==4) {
         double nbfinal =ConvertChoisie.CelciusVersFarenheit(nombre);
         System.out.println(nbfinal);
+        
         }else if (choix==5) {
         double nbfinal =ConvertChoisie.FarenheitVersKelvin(nombre);
         System.out.println(nbfinal);
+        
         }else if (choix==6) {
         double nbfinal =ConvertChoisie.KelvinVersFarenheit(nombre);
         System.out.println(nbfinal);
