c4da4b574fe131dc2b7de9772f9a9e6708e3c4cf
juliettebras <juliettebras@localhost>
2022-11-14 10:22:12 +0100

    Fin sous-classe Baton

--- TP3_BRAS/src/tp3_bras/Baton.java
+++ TP3_BRAS/src/tp3_bras/Baton.java
@@ -9,9 +9,30 @@ package tp3_bras;
  * @author juliettebras
  */
 public class Baton extends Arme {
-    int age;
+    private int age;
     
-public Baton (String Nom, int Nombre,int age){
-     
+public Baton (String Nom, int Nombre,int Age){
+     super(Nom,Nombre);
+     if(Age>0){
+         age=Age;
+     }else{
+         age=0;
+     }
  }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age>0){
+        this.age = age;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Baton{" + "age=" + age + '}';
+    }
+
 }
