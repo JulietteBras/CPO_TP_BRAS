d9d1e1cb40652a57918e841f026e0fe22e49f396
juliettebras <juliettebras@localhost>
2022-11-14 10:14:46 +0100

    Création sous-classe Baton

--- TP3_BRAS/src/tp3_bras/Arme.java
+++ TP3_BRAS/src/tp3_bras/Arme.java
@@ -9,5 +9,44 @@ package tp3_bras;
  * @author juliettebras
  */
 public class Arme {
+    private String nom;
+    private int NiveauAttaque; //Private : Pour ne pas pouvoir modifier la valeur de attaque en dehors de la calsse Arme
+
+public Arme (String Nom, int Nombre){
+    nom=Nom;
+    
+    if (Nombre<0){
+        NiveauAttaque=1;
+    }else{
+        NiveauAttaque=Nombre;
+    if(Nombre>=100){
+        NiveauAttaque=100;
+    }
+    }
+            
+}
+
+    public int getNiveauAttaque() {
+        return NiveauAttaque;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNiveauAttaque(int NiveauAttaque) {
+        if(NiveauAttaque>0){
+         this.NiveauAttaque = NiveauAttaque;   
+        }
+        
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Arme{" + "nom=" + nom + ", NiveauAttaque=" + NiveauAttaque + '}';
+    }
+    
     
 }
---
+++ TP3_BRAS/src/tp3_bras/Baton.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_bras;
+
+/**
+ *
+ * @author juliettebras
+ */
+public class Baton extends Arme {
+    int age;
+    
+public Baton (String Nom, int Nombre,int age){
+     
+ }
+}
